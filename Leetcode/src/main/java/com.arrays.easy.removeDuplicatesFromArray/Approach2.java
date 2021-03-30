9bed6b36ad986520fdae760e37b3c4e82f5fe5ce
Prerna_Gandhi <prernagandhi97@gmail.com>
2021-03-30 10:32:09 +0530

    Remove duplicates from array : Approach2
    Signed-off-by: Prerna_Gandhi <prernagandhi97@gmail.com>

---
+++ Leetcode/src/main/java/com.arrays.easy.removeDuplicatesFromArray/Approach2.java
@@ -0,0 +1,19 @@
+package com.arrays.easy.removeDuplicatesFromArray;
+
+/*
+Time complexity : O(n)
+ */
+public class Approach2 {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length == 0) {
+            return 0;
+        }
+        int counterForUniqueElements = 1;
+        for(int element : nums) {
+            if(nums[counterForUniqueElements - 1] != element){
+                nums[counterForUniqueElements++] = element;
+            }
+        }
+        return counterForUniqueElements;
+    }
+}
