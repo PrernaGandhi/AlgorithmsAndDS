0a7be82fa4f5ea7083a84accf2314e9c1056f387
Prerna_Gandhi <prernagandhi97@gmail.com>
2021-03-30 12:06:27 +0530

    Contains Duplicates : Approach2
    Signed-off-by: Prerna_Gandhi <prernagandhi97@gmail.com>

---
+++ Leetcode/src/main/java/com/arrays/easy/containsDuplicates/Approach2.java
@@ -0,0 +1,23 @@
+package com.arrays.easy.containsDuplicates;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+Time complexity: O(n log n)
+ */
+public class Approach2 {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> list = new HashSet<>();
+        Arrays.sort(nums);
+        for(int num : nums) {
+            if(list.contains(num)) {
+                return true;
+            } else {
+                list.add(num);
+            }
+        }
+        return false;
+    }
+}
