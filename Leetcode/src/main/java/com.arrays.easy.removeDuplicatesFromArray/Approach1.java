98cd18a723907c1dc96de41f28d431033876ceed
Prerna_Gandhi <prernagandhi97@gmail.com>
2021-03-30 10:31:56 +0530

    Remove duplicates from array : Approach1
    Signed-off-by: Prerna_Gandhi <prernagandhi97@gmail.com>

---
+++ Leetcode/src/main/java/com.arrays.easy.removeDuplicatesFromArray/Approach1.java
@@ -0,0 +1,32 @@
+package com.arrays.easy.removeDuplicatesFromArray;
+
+/*
+Time complexity : O(n)
+ */
+public class Approach1 {
+    public int removeDuplicates(int[] nums) {
+        int count = 0;
+        int j;
+        if(nums.length == 0) {
+            return 0;
+        }
+        if(nums.length == 1) {
+            return 1;
+        }
+        for(int i = 0; i < nums.length; i++) {
+            j = i + 1;
+            if(j < nums.length && nums[i] != nums[j]) {
+                nums[count++] = nums[i];
+            }
+        }
+        if (count == 0) {
+            nums[count++] = nums[nums.length - 1];
+            return count;
+        }
+        if (nums[count - 1] != nums[nums.length - 1] ){
+            nums[count++] = nums[nums.length - 1];
+            return count;
+        }
+        return count;
+    }
+}
