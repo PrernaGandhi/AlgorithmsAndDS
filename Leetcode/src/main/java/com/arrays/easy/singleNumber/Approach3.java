9ea1f017566f64586d6b6aac9840cfcdea035826
Prerna_Gandhi <prernagandhi97@gmail.com>
2021-03-31 13:15:43 +0530

    Single Number : refactored Approach3 to use diamond operator and amended comments
    Signed-off-by: Prerna_Gandhi <prernagandhi97@gmail.com>

--- Leetcode/src/main/java/com/arrays/easy/singleNumber/Approach3.java
+++ Leetcode/src/main/java/com/arrays/easy/singleNumber/Approach3.java
@@ -7,15 +7,15 @@ import java.util.Set;
 Using math :
 2 * (a + b + c) - (a + a + b + b + c) = c
 
-Time complexity : O(n + n) = O(n)O(n+n)=O(n). sum will call next to iterate through \text{nums}nums. We can see it as sum(list(i, for i in nums)) which means the time complexity is O(n)O(n) because of the number of elements(nn) in \text{nums}nums.
+Time complexity : O(n+n)=O(n). sum will call next to iterate through nums. We can see it as sum(list(i, for i in nums)) which means the time complexity is O(n) because of the number of elements(n) in nums.
 
-Space complexity : O(n + n) = O(n)O(n+n)=O(n). set needs space for the elements in nums
+Space complexity : O(n+n)=O(n). set needs space for the elements in nums
 
  */
 public class Approach3 {
     public int singleNumber(int[] nums) {
         int sumOfSet = 0, sumOfNums = 0;
-        Set<Integer> set = new HashSet();
+        Set<Integer> set = new HashSet<>();
 
         for (int num : nums) {
             if (!set.contains(num)) {
